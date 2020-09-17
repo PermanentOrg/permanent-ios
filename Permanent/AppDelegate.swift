9afb386e4778d05ec7b26bda6a93cc9cf71cf2a2
Adrian Creteanu <adrian.creteanu@vspartners.us>
2020-09-17 11:36:53 +0300

    - Small change of app structure

--- Permanent/AppDelegate.swift
+++ Permanent/AppDelegate.swift
@@ -6,44 +6,44 @@
 //  Copyright © 2020 Lucian Cerbu. All rights reserved.
 //
 
-import UIKit
 import Firebase
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  
-  var window: UIWindow?
-  
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    window = UIWindow(frame: UIScreen.main.bounds)
-    let mainNavicationController = UINavigationController()
-    let loginViewController = SignUpViewController.init(nibName: "SignUpViewController", bundle: .main)
-  
-    if UserDefaultsService.shared.isNewUser() {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainNavicationController = UINavigationController()
+        let loginViewController = SignUpViewController(nibName: "SignUpViewController", bundle: .main)
+
+        if UserDefaultsService.shared.isNewUser() {
             let onboardingView = UIStoryboard(name: "Onboarding", bundle: .main).instantiateViewController(withIdentifier: "Onboarding")
             mainNavicationController.viewControllers = [onboardingView]
         } else {
             mainNavicationController.viewControllers = [loginViewController]
         }
 
-    window?.rootViewController = mainNavicationController
-    window?.makeKeyAndVisible()
-    
-    initFirebase()
+        window?.rootViewController = mainNavicationController
+        window?.makeKeyAndVisible()
 
-    return true
-  }
-  
-  fileprivate func initFirebase() {
-      guard
-        let infoDict = Bundle.main.infoDictionary,
-        let fileName = infoDict["GOOGLE_PLIST_NAME"] as? String,
-        let filePath = Bundle.main.path(forResource: fileName, ofType: "plist"),
-        let fileOpts = FirebaseOptions(contentsOfFile: filePath) else {
-          assert(false, "Cannot load config file")
-      }
-    
-      FirebaseApp.configure(options: fileOpts)
-  }
+        initFirebase()
+
+        return true
+    }
+
+    fileprivate func initFirebase() {
+        guard
+            let infoDict = Bundle.main.infoDictionary,
+            let fileName = infoDict["GOOGLE_PLIST_NAME"] as? String,
+            let filePath = Bundle.main.path(forResource: fileName, ofType: "plist"),
+            let fileOpts = FirebaseOptions(contentsOfFile: filePath)
+        else {
+            assert(false, "Cannot load config file")
+            return
+        }
+
+        FirebaseApp.configure(options: fileOpts)
+    }
 }
-
