72821ad140964ad4e9e978ce36884913cec3ffb8
Vlad Rusu <v@vladr.com>
2021-02-26 10:54:02 +0200

    - removed duplicate code

--- Permanent/ViewControllers/FilePreviewViewController.swift
+++ Permanent/ViewControllers/FilePreviewViewController.swift
@@ -40,34 +40,23 @@ class FilePreviewViewController: BaseViewController<FilePreviewViewModel> {
             self.recordVO = record
             
             if let localURL = self.fileHelper.url(forFileNamed: record?.recordVO?.uploadFileName ?? ""),
-               let contentType = record?.recordVO?.fileVOS?.first?.contentType
-            {
+               let contentType = record?.recordVO?.fileVOS?.first?.contentType {
                 switch self.file.type {
                 case FileType.image:
-                    let imageTag = "<body style=\"width:\(self.view.frame.width * UIScreen.main.scale);height:\(self.view.frame.height * UIScreen.main.scale);background-color:#000000;\"><img src=\"\(localURL)\" style=\"max-width:\(self.view.frame.width * UIScreen.main.scale);max-height:\(self.view.frame.height * UIScreen.main.scale);margin:0;padding:0;position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);\"/></body>"
-                    let htmlBody = "<html><body style=\"width:100%;height:100%;background-color:#000000;margin:0;padding:0;\">\(imageTag)</body></html>"
-                    self.webView.loadHTMLString(htmlBody, baseURL: localURL)
+                    self.loadImage(withURL: localURL, contentType: contentType)
                 case FileType.video:
-                    let videoTag = "<video style=\"max-width:100%;max-height:100%;margin:50% auto 0 auto;padding:0;\" controls autoplay> <source src=\"\(localURL)\" type=\"\(contentType)\">Your browser does not support the video tag.</video>"
-                    let htmlBody = "<html><body style=\"width:100%;height:100%;background-color:#000000;margin:0;padding:0;\">\(videoTag)</body></html>"
-                    self.webView.loadHTMLString(htmlBody, baseURL: localURL)
+                    self.loadVideo(withURL: localURL, contentType: contentType)
                 default:
                     self.webView.loadFileURL(localURL, allowingReadAccessTo: localURL)
                 }
-            
             } else if let downloadURLString = record?.recordVO?.fileVOS?.first?.downloadURL,
                       let contentType = record?.recordVO?.fileVOS?.first?.contentType,
-                      let downloadURL = URL(string: downloadURLString)
-            {
+                      let downloadURL = URL(string: downloadURLString) {
                 switch self.file.type {
                 case FileType.image:
-                    let imageTag = "<body style=\"width:\(self.view.frame.width * UIScreen.main.scale);height:\(self.view.frame.height * UIScreen.main.scale);background-color:#000000;\"><img src=\"\(downloadURLString)\" style=\"max-width:\(self.view.frame.width * UIScreen.main.scale);max-height:\(self.view.frame.height * UIScreen.main.scale);margin:0;padding:0;position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);\"/></body>"
-                    let htmlBody = "<html><body style=\"width:100%;height:100%;background-color:#000000;margin:0;padding:0;\">\(imageTag)</body></html>"
-                    self.webView.loadHTMLString(htmlBody, baseURL: nil)
+                    self.loadImage(withURL: downloadURL, contentType: contentType)
                 case FileType.video:
-                    let videoTag = "<video style=\"max-width:100%;max-height:100%;margin:50% auto 0 auto;padding:0;\" controls autoplay> <source src=\"\(downloadURLString)\" type=\"\(contentType)\">Your browser does not support the video tag.</video>"
-                    let htmlBody = "<html><body style=\"width:100%;height:100%;background-color:#000000;margin:0;padding:0;\">\(videoTag)</body></html>"
-                    self.webView.loadHTMLString(htmlBody, baseURL: nil)
+                    self.loadVideo(withURL: downloadURL, contentType: contentType)
                 default:
                     let request = URLRequest(url: downloadURL)
                     self.webView.load(request)
@@ -92,6 +81,21 @@ class FilePreviewViewController: BaseViewController<FilePreviewViewModel> {
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: leftButtonImage, style: .plain, target: self, action: #selector(closeButtonAction(_:)))
         
         navigationItem.title = file.name
+    }
+    
+    func htmlBody(withContent content: String) -> String {
+        return "<html><body style=\"width:100%;height:100%;background-color:#000000;margin:0;padding:0;\">\(content)</body></html>"
+    }
+    
+    func loadImage(withURL url: URL, contentType: String) {
+        let imageTag = "<body style=\"width:100%;height:100%;background-color:#000000;\"><img src=\"\(url)\" style=\"max-width:100%;max-height:100%;margin:50% auto 0 auto;padding:0;\"/></body>"
+        
+        webView.loadHTMLString(htmlBody(withContent: imageTag), baseURL: url)
+    }
+    
+    func loadVideo(withURL url: URL, contentType: String) {
+        let videoTag = "<video style=\"max-width:100%;max-height:100%;margin:50% auto 0 auto;padding:0;\" controls autoplay> <source src=\"\(url)\" type=\"\(contentType)\">Your browser does not support the video tag.</video>"
+        webView.loadHTMLString(htmlBody(withContent: videoTag), baseURL: url)
     }
     
     func share(url: URL) {
@@ -140,7 +144,6 @@ extension FilePreviewViewController: WKNavigationDelegate {
     }
 
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-        // hide spinner for audio files
         hideSpinner()
     }
 }
