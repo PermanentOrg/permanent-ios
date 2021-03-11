410674b2aecb3c3134287422a5f392ddd7dfc364
Lucian Cerbu <lucian.cerbu@vspartners.us>
2021-03-11 14:23:49 +0200

    Removed Uploaded By field

--- Permanent/ViewControllers/FileDetailsViewController.swift
+++ Permanent/ViewControllers/FileDetailsViewController.swift
@@ -7,25 +7,23 @@
 
 import UIKit
 
-let reuseIdentifier = "CellIdentifer";
+let reuseIdentifier = "CellIdentifer"
 
 class FileDetailsViewController: BaseViewController<FilePreviewViewModel> {
-    
     var file: FileViewModel!
     var recordVO: RecordVOData!
     let infoSubmenuItems: [String] = ["Name", "Description", "Date", "Location", "Tags"]
-    let detailsSubmenuItems: [String] = ["Uploaded", "Uploaded By","Last Modified","Created","File Created","Size","File Type","Original File Name:","Original File Type"]
+    let detailsSubmenuItems: [String] = ["Uploaded", "Last Modified", "Created", "File Created", "Size", "File Type", "Original File Name:", "Original File Type"]
     var infoDetailsCellNumber: [Int]!
     var currentSubmenuSelection = 0
     @IBOutlet var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        showSpinner()
-        self.initUI()
+        initUI()
 
-let layout = UICollectionViewFlowLayout()
-layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45)
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45)
 
         collectionView.collectionViewLayout = layout
         collectionView.backgroundColor = .black
@@ -35,23 +33,21 @@ layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.mai
         collectionView.register(FileDetailsBottomCollectionViewCell.nib(), forCellWithReuseIdentifier: FileDetailsBottomCollectionViewCell.identifier)
         collectionView.register(SaveButtonCollectionViewCell.nib(), forCellWithReuseIdentifier: SaveButtonCollectionViewCell.identifier)
         
+        showSpinner()
+        
         viewModel = FilePreviewViewModel(file: file)
         viewModel?.getRecord(file: file, then: { record in
             self.recordVO = record?.recordVO
-                self.hideSpinner()
-
-        
+            self.hideSpinner()
 
             self.collectionView.delegate = self
             self.collectionView.dataSource = self
             
         })
-
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        updateSpinner(isLoading: true)
     }
     
     func initUI() {
@@ -71,7 +67,7 @@ layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.mai
         
         navigationItem.title = file.name
         
-        infoDetailsCellNumber = [infoSubmenuItems.count + 1,detailsSubmenuItems.count]
+        infoDetailsCellNumber = [infoSubmenuItems.count + 1, detailsSubmenuItems.count]
     }
     
     override func styleNavBar() {
@@ -97,9 +93,8 @@ layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.mai
 
 extension FileDetailsViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if section == 1
-        {
-           return infoDetailsCellNumber[currentSubmenuSelection]
+        if section == 1 {
+            return infoDetailsCellNumber[currentSubmenuSelection]
         }
         return 2
     }
@@ -109,8 +104,7 @@ extension FileDetailsViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-       // let cell: UICollectionViewCell!
+        // let cell: UICollectionViewCell!
         
         updateSpinner(isLoading: false)
         
@@ -131,13 +125,13 @@ extension FileDetailsViewController: UICollectionViewDataSource {
             }
         } else {
             if indexPath.item < 5 || currentSubmenuSelection == 1 {
-            let cell3 = collectionView.dequeueReusableCell(withReuseIdentifier: FileDetailsBottomCollectionViewCell.identifier, for: indexPath) as! FileDetailsBottomCollectionViewCell
-            if currentSubmenuSelection == 0 && indexPath.item < 2 {
-                cell3.configure(title: cellTitle(itemNumber: indexPath.item), details: cellDetails(itemNumber: indexPath.item),isDetailsFieldEditable: true)
-            } else {
-                cell3.configure(title: cellTitle(itemNumber: indexPath.item), details: cellDetails(itemNumber: indexPath.item))
-            }
-            return cell3
+                let cell3 = collectionView.dequeueReusableCell(withReuseIdentifier: FileDetailsBottomCollectionViewCell.identifier, for: indexPath) as! FileDetailsBottomCollectionViewCell
+                if currentSubmenuSelection == 0, indexPath.item < 2 {
+                    cell3.configure(title: cellTitle(itemNumber: indexPath.item), details: cellDetails(itemNumber: indexPath.item), isDetailsFieldEditable: true)
+                } else {
+                    cell3.configure(title: cellTitle(itemNumber: indexPath.item), details: cellDetails(itemNumber: indexPath.item))
+                }
+                return cell3
             } else {
                 let cell4 = collectionView.dequeueReusableCell(withReuseIdentifier: SaveButtonCollectionViewCell.identifier, for: indexPath) as! SaveButtonCollectionViewCell
                 cell4.configure(title: .save)
@@ -145,12 +139,10 @@ extension FileDetailsViewController: UICollectionViewDataSource {
             }
         }
         
-        
 //        cell.rightButtonTapAction = { _ in
 //            self.handleCellRightButtonAction(for: file, atIndexPath: indexPath)
 //        }
     }
-    
     
     func cellTitle(itemNumber: Int) -> String {
         if currentSubmenuSelection == 0 {
@@ -158,66 +150,67 @@ extension FileDetailsViewController: UICollectionViewDataSource {
         }
         return detailsSubmenuItems[itemNumber]
     }
+
     func cellDetails(itemNumber: Int) -> String {
         var details: String!
-        switch itemNumber {
-        case 0:
-            if currentSubmenuSelection == 0 {
-                details = self.recordVO.displayName
+        switch (itemNumber, currentSubmenuSelection) {
+        case (0, 0):
+            details = recordVO.displayName
+        case (0, 1):
+            details = convertDateFormater(recordVO.createdDT ?? "")
+        case (1, 0):
+            details = recordVO.recordVODescription ?? ""
+        case (1, 1):
+            details = convertDateFormater(recordVO.updatedDT ?? "")
+        case (2, 0):
+            details = convertDateFormater(recordVO.displayDT ?? "")
+        case (2, 1):
+            details = convertDateFormater(recordVO.derivedDT ?? "")
+        case (3, 0):
+            if
+                let street = recordVO.locnVO?.streetName,
+                // let city = self.recordVO.locnVO?.locality,
+                let country = recordVO.locnVO?.country
+            {
+                details = "\(street),\(country)"
             } else {
-                details = convertDateFormater(self.recordVO.displayDT ?? "")
+                details = ""
             }
-        case 1:
-            if currentSubmenuSelection == 0 {
-                details = self.recordVO.recordVODescription ?? ""
-            } else {
-                details = String(self.recordVO.uploadAccountID!)
-            }
-        case 2:
-            if currentSubmenuSelection == 0 {
-                details = convertDateFormater(self.recordVO.displayDT ?? "")
-            } else {
-                details = convertDateFormater(self.recordVO.updatedDT ?? "")
-            }
-        case 3:
-            if currentSubmenuSelection == 0 {
-                if
-                    let street = self.recordVO.locnVO?.streetName,
-                    //let city = self.recordVO.locnVO?.locality,
-                    let country = self.recordVO.locnVO?.country {
-                    details = "\(street),\(country)"
-                }
-                else {
-                    details = ""
-                }
-            } else {
-                details = convertDateFormater(self.recordVO.createdDT ?? "")
-            }
-        case 4:
-            if currentSubmenuSelection == 0 {
+        case (3, 1):
+            details = convertDateFormater(recordVO.derivedCreatedDT ?? "")
+        case (4, 0):
+
+            if recordVO.tagVOS?.count != 0 {
                 details = ""
             } else {
-                details = convertDateFormater(self.recordVO.derivedDT ?? "")
+                details = "(none)"
             }
-        case 5:
-            details = ByteCountFormatter.string(fromByteCount: Int64(self.recordVO.size ?? 0), countStyle: .file)
-        case 6:
-            details = URL(string: self.recordVO.type)?.pathExtension
-        case 7:
-            details = URL(string: self.recordVO.uploadFileName)?.deletingPathExtension().absoluteString
-        case 8:
-            details = URL(string: self.recordVO.uploadFileName)?.pathExtension
+        case (4, 1):
+            details = ByteCountFormatter.string(fromByteCount: Int64(recordVO.size ?? 0), countStyle: .file)
+        case (5, _):
+            details = URL(string: recordVO.type)?.pathExtension
+        case (6, _):
+            details = URL(string: recordVO.uploadFileName)?.deletingPathExtension().absoluteString
+        case (7, _):
+            details = URL(string: recordVO.uploadFileName)?.pathExtension
+        case (8, _):
+            details = ""
         default:
             details = ""
         }
         return details
     }
+
     func convertDateFormater(_ date: String) -> String {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         if let dateProcessed = dateFormatter.date(from: date) {
-            dateFormatter.dateFormat = "yyyy-MM-dd h:mm:ss a"
-            return  dateFormatter.string(from: dateProcessed)
+            dateFormatter.dateFormat = "yyyy-MM-dd h:mm a"
+            if currentSubmenuSelection == 0 {
+                return dateFormatter.string(from: dateProcessed) + " GMT"
+            } else {
+                return dateFormatter.string(from: dateProcessed)
+            }
         } else {
             return ""
         }
@@ -228,8 +221,8 @@ extension FileDetailsViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
         
-        if indexPath == [0,0] {
-            let fileDetailsVC = UIViewController.create(withIdentifier: .filePreview , from: .main) as! FilePreviewViewController
+        if indexPath == [0, 0] {
+            let fileDetailsVC = UIViewController.create(withIdentifier: .filePreview, from: .main) as! FilePreviewViewController
             fileDetailsVC.file = file
             
             navigationController?.setViewControllers([fileDetailsVC], animated: false)
@@ -237,30 +230,18 @@ extension FileDetailsViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        switch (indexPath.section, indexPath.row) {
-        case (0,0):
+        switch (indexPath.section, indexPath.row, currentSubmenuSelection) {
+        case (0, 0, _):
             return CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45)
-        case (0,1):
+        case (0, 1, _):
             return CGSize(width: UIScreen.main.bounds.width, height: 40)
-        case (1,5):
+        case (1, 5, 0):
             return CGSize(width: UIScreen.main.bounds.width, height: 40)
-        case (1,_):
+        case (1, _, _):
             return CGSize(width: UIScreen.main.bounds.width, height: 65)
         default:
             return CGSize(width: UIScreen.main.bounds.width, height: 10)
         }
-        
-//        if indexPath[0] == 0 {
-//            if indexPath.item == 0 {
-//                return CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.45)
-//            } else if indexPath.item == 1 {
-//                return CGSize(width: UIScreen.main.bounds.width, height: 40)
-//            }
-//        } else if indexPath[0] == 1 {
-//            return CGSize(width: UIScreen.main.bounds.width, height: 100)
-//        }
-//        return CGSize(width: UIScreen.main.bounds.width, height: 10)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
@@ -275,6 +256,7 @@ extension FileDetailsViewController: UICollectionViewDelegateFlowLayout {
         return UIEdgeInsets(top: 10, left: 0, bottom: 5, right: 0)
     }
 }
+
 extension FileDetailsViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         (textField as? TextField)?.toggleBorder(active: false)
