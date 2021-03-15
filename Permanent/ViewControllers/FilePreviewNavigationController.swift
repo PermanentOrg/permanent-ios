73d5e74e9c50ed7774cff64efd517ef2e5aeace3
Vlad Rusu <v@vladr.com>
2021-03-15 14:09:02 +0200

    VSP-246 [iOS] After editing a file's meta data, when returning back to the list of files refresh the page
    - created custom navigation controller for file preview screens
    - file preview navigation has delegate to main/shares screen
    - file details notifies file preview navigation when a change occured
    - file preview navigation notifies main/shares when a change occured in order to reload data.
    - file preview and file details now share the same instance of view model, in order to reduce loading time when navigating between the two screens

---
+++ Permanent/ViewControllers/FilePreviewNavigationController.swift
@@ -0,0 +1,65 @@
+//
+//  FilePreviewNavigationController.swift
+//  Permanent
+//
+//  Created by Vlad Alexandru Rusu on 15.03.2021.
+//
+
+import UIKit
+
+protocol FilePreviewNavigationControllerDelegate: class {
+    func filePreviewNavigationControllerWillClose(_ filePreviewNavigationVC: FilePreviewNavigationController, hasChanges: Bool)
+}
+
+class FilePreviewNavigationController: UINavigationController {
+    
+    weak var filePreviewNavDelegate: FilePreviewNavigationControllerDelegate?
+    
+    var hasChanges: Bool = false
+    
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+        
+        addCloseButton(toViewController: rootViewController)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setViewControllers(_ viewControllers: [UIViewController], animated: Bool) {
+        super.setViewControllers(viewControllers, animated: animated)
+        
+        if let vc = viewControllers.first {
+            addCloseButton(toViewController: vc)
+        }
+    }
+    
+    func addCloseButton(toViewController vc: UIViewController) {
+        let leftButtonImage: UIImage!
+        if #available(iOS 13.0, *) {
+            leftButtonImage = UIImage(systemName: "xmark", withConfiguration: UIImage.SymbolConfiguration(weight: .regular))
+        } else {
+            leftButtonImage = UIImage(named: "close")
+        }
+        
+        vc.navigationItem.leftBarButtonItem = UIBarButtonItem(image: leftButtonImage, style: .plain, target: self, action: #selector(closeButtonAction(_:)))
+    }
+    
+    @objc private func closeButtonAction(_ sender: Any) {
+        filePreviewNavDelegate?.filePreviewNavigationControllerWillClose(self, hasChanges: hasChanges)
+        
+        if let topVC = topViewController as? FilePreviewViewController {
+            topVC.willClose()
+        } else if let topVC = topViewController as? FileDetailsViewController {
+            topVC.willClose()
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+
+}
