262b08bbc24a8ffaf70f544bce15986777f1cec2
Lucian Cerbu <lucian.cerbu@vspartners.us>
2021-05-12 17:23:39 +0300

    Added init for iOS 12 using nibName.

--- Permanent/ViewControllers/RootNavigationController.swift
+++ Permanent/ViewControllers/RootNavigationController.swift
@@ -18,6 +18,10 @@ class RootNavigationController: UINavigationController {
         configureNavigationItems()
     }
     
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
