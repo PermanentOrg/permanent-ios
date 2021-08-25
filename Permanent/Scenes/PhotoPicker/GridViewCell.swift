94da65a935de37a1e7261fd3610d9c4843369d70
Vlad Rusu <v@vladr.com>
2021-08-25 12:07:32 +0300

    VSP-394 [iOS] Multi select uploads

---
+++ Permanent/Scenes/PhotoPicker/GridViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  GridViewCell.swift
+//  Permanent
+//
+//
+
+import UIKit
+
+class GridViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var representedAssetIdentifier: String!
+    
+    var thumbnailImage: UIImage! {
+        didSet {
+            imageView.image = thumbnailImage
+        }
+    }
+    
+    @IBOutlet weak var selectedImage: UIImageView!
+    @IBOutlet weak var selectedView: UIView!
+    
+    override var isSelected: Bool {
+        didSet {
+            selectedImage.isHidden = !isSelected
+            selectedView.isHidden = !isSelected
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+    
+    
+}
