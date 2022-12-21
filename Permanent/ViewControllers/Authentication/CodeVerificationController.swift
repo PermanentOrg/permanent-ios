18838534fee8f6eab6fd52d9bc88014bd88eba23
Lucian Cerbu <lucian.cerbu@vspartners.us>
2022-12-21 16:01:57 +0200

    Removed unnecessary methods.

--- Permanent/ViewControllers/Authentication/CodeVerificationController.swift
+++ Permanent/ViewControllers/Authentication/CodeVerificationController.swift
@@ -19,14 +19,8 @@ class CodeVerificationController: BaseViewController<AuthViewModel> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupNotifications()
+
         initUI()
-    }
-    
-    func setupNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     fileprivate func initUI() {
@@ -56,9 +50,7 @@ class CodeVerificationController: BaseViewController<AuthViewModel> {
         let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
         let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(confirmAction(_:)) )
         toolBar.setItems([flexibleSpace,doneButton], animated: false)
-        
-        
-        codeField.delegate = self
+
         codeField.inputAccessoryView = toolBar
         
         addDismissKeyboardGesture()
@@ -98,23 +90,5 @@ class CodeVerificationController: BaseViewController<AuthViewModel> {
         case .error(let message):
             showAlert(title: .error, message: message)
         }
-    }
-    
-    // MARK: - Keyboard
-    
-    @objc func keyboardWillShow(_ notification: Notification) {
-        let point = CGPoint(x: 0, y: codeField.frame.origin.y - 10)
-        scrollView.setContentOffset(point, animated: true)
-    }
-    
-    @objc func keyboardWillHide(_ notification: Notification) {
-        let point = CGPoint(x: 0, y: 0)
-        scrollView.setContentOffset(point, animated: true)
-    }
-}
-
-extension CodeVerificationController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        return true
     }
 }
