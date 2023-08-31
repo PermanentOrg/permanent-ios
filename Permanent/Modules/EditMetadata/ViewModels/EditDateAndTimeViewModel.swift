4165c3b8bd18e127831f888981e053625f6ec7a2
Lucian Cerbu <lucian.cerbu@vspartners.us>
2023-08-31 13:14:07 +0300

    Added backend functionality for adding dates to selected files.

--- Permanent/Modules/EditMetadata/ViewModels/EditDateAndTimeViewModel.swift
+++ Permanent/Modules/EditMetadata/ViewModels/EditDateAndTimeViewModel.swift
@@ -14,6 +14,8 @@ class EditDateAndTimeViewModel: ObservableObject {
     @Published var changesConfirmed: Bool = false
     @Published var selectedDate = Date()
     @Published var showConfirmation:Bool = false
+    @Published var changesWereSaved: Bool = false
+    @Published var showAlert: Bool = false
     
     let startingDate: Date = Calendar.current.date(from: DateComponents(year: 1900)) ?? Date()
     let endingDate: Date = Date()
@@ -24,6 +26,32 @@ class EditDateAndTimeViewModel: ObservableObject {
     }
     
     func applyChanges() {
-        ///To do: Implement date change
+        isLoading = true
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        let date = dateFormatter.string(from: selectedDate)
+        
+        let updatedFiles = selectedFiles.map { file in
+            var newFile = file
+            newFile.date = date
+            return newFile
+        }
+        
+        let apiOperation = APIOperation(FilesEndpoint.multipleFilesUpdate(files: updatedFiles))
+        
+        apiOperation.execute(in: APIRequestDispatcher()) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.isLoading = false
+                switch result {
+                case .json( _, _):
+                    self?.hasUpdates.wrappedValue = true
+                    self?.changesWereSaved = true
+                case .error(_, _):
+                    self?.showAlert = true
+                default:
+                    self?.showAlert = true
+                }
+            }
+        }
     }
 }
