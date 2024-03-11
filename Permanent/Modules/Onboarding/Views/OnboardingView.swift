3435323c5b2ccdfdff798d35143d120d0484d74a
Lucian Cerbu <lucian.cerbu@vspartners.us>
2024-03-11 14:41:23 +0200

    Added common elements to the container view.

--- Permanent/Modules/Onboarding/Views/OnboardingView.swift
+++ Permanent/Modules/Onboarding/Views/OnboardingView.swift
@@ -7,18 +7,46 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    enum ContentType {
+        case none, welcome
+    }
+    
+    @State private var contentType: ContentType = .welcome
+    
     var body: some View {
         ZStack {
             Gradient.darkLightBlueGradient
-            VStack {
-                Text("Onboarding View")
-                    .foregroundColor(.white)
+            VStack() {
+                HStack() {
+                    Image(.simpleLogo)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 40, height: 30)
+                        .clipped()
+                    Spacer()
+                }
+                HStack(spacing: 8) {
+                    DividerSmallBar(type: .gradient)
+                    DividerSmallBar(type: .empty)
+                    DividerSmallBar(type: .empty)
+                }
+                .padding(.top, 24)
+                .padding(.bottom, 32)
+                
+                if contentType != .none {
+                    switch contentType {
+                    case .welcome:
+                        OnboardingWelcomeView()
+                    default:
+                        Spacer()
+                    }
+                }
+                Spacer()
             }
+            .padding(.horizontal, 32)
+            .padding(.top, 70)
+            .padding(.bottom, 40)
         }
         .ignoresSafeArea()
     }
-}
-
-#Preview {
-    OnboardingView()
 }
---
+++ Permanent/Modules/Onboarding/Views/OnboardingWelcomeView.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingWelcomeView.swift
+//  Permanent
+//
+//  Created by Lucian Cerbu on 11.03.2024.
+
+import SwiftUI
+
+struct OnboardingWelcomeView: View {
+    var body: some View {
+        Text("")
+            .foregroundColor(.white)
+    }
+}
